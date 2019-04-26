413925bcd5c50be63d044a4aa763116908185cdd
emorgan00 <emorgan00@stuy.edu>
2019-04-25 20:58:21 -0400

    basic stuff

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,20 @@
 import os
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 
-	return 'this is a test'
+	return redirect(url_for('create'))
+
+@app.route('/create', methods = ('GET', 'POST'))
+def create():
+
+	if request.method == 'GET':
+
+		return render_template('create.html')
+
+	elif request.method == 'POST':
+
+		return render_template('display.html', variant = request.form['variant'])
